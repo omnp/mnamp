770a7eb7298877d36d0e58e377974d08114c74bc
Olli M <40587954+omnp@users.noreply.github.com>
2024-06-27 21:25:27 +0300

    Update process-cffi

--- util/process-cffi.py
+++ util/process-cffi.py
@@ -39,12 +39,12 @@ import matplotlib.pyplot as plot
 n = 48000#4096
 sr = float(n)
 f = 50.0
-sine = numpy.array(([math.sin(math.pi * f * 2.0 * (i/n))/f for i in range(0,n+1,1)]))
+sine = numpy.array(([0.0 for _ in range(n)] + [math.sin(math.pi * f * 2.0 * (i/n))/f for i in range(0,n+1,1)]))
 #delta = 10.0
 #for _ in range(1,64):
 #    f += delta
 #    sine += numpy.array(([math.sin(math.pi * f * 2.0 * (i/n))/f for i in range(0,n+1,1)]))
-out = array.array('f', [0.0,]*(n+1))
+out = array.array('f', [0.0,]*(2*n+1))
 amp = descriptor.instantiate(ffi.cast('LV2_Descriptor *', descriptor), sr, ffi.cast('const char *', 0), ffi.cast('const LV2_Feature * const*', 0))
 buffer_length = 32
 ports = {
@@ -80,17 +80,17 @@ descriptor.connect_port(amp, 12, ports['compensation'])
 descriptor.connect_port(amp, 13, ports['volume'])
 descriptor.activate(amp)
 ports['gain1'][0] = ffi.cast('float', 0.0)
-ports['gain2'][0] = ffi.cast('float', 24.0)
+ports['gain2'][0] = ffi.cast('float', 48.0)
 ports['toggle'][0] = ffi.cast('float', 1.0)
-ports['cutoff'][0] = ffi.cast('float', 1200.0)
+ports['cutoff'][0] = ffi.cast('float', 900.0)
 ports['stages'][0] = ffi.cast('float', ffi.cast('unsigned int', 2))
-ports['bias'][0] = ffi.cast('float', 0.368)
-ports['resonance'][0] = ffi.cast('float', 0.501)
-ports['factor'][0] = ffi.cast('float', ffi.cast('unsigned int', 128))
-ports['eps'][0] = ffi.cast('float', 0.7)
-ports['eq'][0] = ffi.cast('float', 0.7)
-ports['compensation'][0] = ffi.cast('float', 20.0)
-ports['volume'][0] = ffi.cast('float', -20.0)
+ports['bias'][0] = ffi.cast('float', 0.468)
+ports['resonance'][0] = ffi.cast('float', 0.707)
+ports['factor'][0] = ffi.cast('float', ffi.cast('unsigned int', 2))
+ports['eps'][0] = ffi.cast('float', 0.707)
+ports['eq'][0] = ffi.cast('float', 0.509)
+ports['compensation'][0] = ffi.cast('float', -0.0)
+ports['volume'][0] = ffi.cast('float', -0.0)
 for i in range(0,len(sine)-1,buffer_length):
     for j in range(0, buffer_length):
         ports['in'][j] = sine[i+j]
@@ -103,7 +103,7 @@ plot.figure()
 plot.plot(range(len(sine)), sine)
 plot.plot(range(len(out)), out)
 plot.show()
-F = numpy.fft.fft(out,n)
+F = numpy.fft.fft(out[n:],n)
 F = F[:len(F)//2]
 plot.figure()
 plot.plot([i for i in range(len(F))], numpy.abs(F)/n)
